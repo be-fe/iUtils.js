f45a000d233e0c32167a47852f04391923d7da4c
leiquan <leiquan@baidu.com>
2015-12-06 18:15:53 +0800

    完善了树形选择

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -2,20 +2,20 @@
  * Created by leiquan on 15/11/30.
  */
 
-var submit = document.querySelector('#submit');
-
-var modules = document.querySelectorAll('#modules li input');
-
-submit.onclick = function () {
-
-    var arr = [];
-    for (var i = 0; i < modules.length; i++) {
-        if (modules[i].checked) {
-            arr.push(modules[i].id);
-        }
-    }
-
-    console.log("勾选的组件为:" + arr);
-    window.location.href = "/build?module=" + arr.join(',');
-
-}
+//var submit = document.querySelector('#submit');
+//
+//var modules = document.querySelectorAll('#input[type=checkbox]');
+//
+//submit.onclick = function () {
+//
+//    //var arr = [];
+//    //for (var i = 0; i < modules.length; i++) {
+//    //    if (modules[i].checked) {
+//    //        arr.push(modules[i].id);
+//    //    }
+//    //}
+//
+//    console.log("勾选的组件为:" + modules);
+//    // window.location.href = "/build?module=" + arr.join(',');
+//
+//}
