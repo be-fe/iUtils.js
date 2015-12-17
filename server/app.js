5954ef5a7217a30d0cec499a625b9fcee59079f2
leiquan <leiquan@baidu.com>
2015-12-17 16:33:28 +0800

    up

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -38,7 +38,7 @@ app.use('/content', contentRoutes);
 app.use('/jsonp', jsonpRoutes);
 
 // 调试信息
-app.use(logger('dev'));
+//app.use(logger('dev'));
 
 // 捕获404错误并且传递错误
 app.use(function (req, res, next) {
