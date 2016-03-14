761a4f3137182105d6175bc65d0bb73a4c48f7dd
leiquan <leiquan@live.com>
2016-03-14 17:02:19 +0800

    添加了按照平台打包

---
+++ server/config.node.js
@@ -0,0 +1,26 @@
+// 请注释掉不需要的模块
+module.exports = [
+
+    'array/arrayEqual',
+    'array/arrayOrderByMax',
+    'array/arrayOrderByMin',
+    'array/arrayRemove',
+    'array/indexOf',
+
+    'fs/mkdirsSync',
+    'fs/scanFolder',
+
+    'object/deepCopy',
+    'object/extend',
+    'random/randomColor',
+    'random/randomNumber',
+    'regexp/isEmail',
+    'regexp/isHexAdecimal',
+    'regexp/isHexColor',
+    'regexp/isTimeString',
+    'regexp/isUrl',
+    'string/trim',
+    'time/parseTime',
+    'trigger/trigger',
+    'type/getType'
+];
--- server/routes/build.js
+++ server/routes/build.js
@@ -4,12 +4,17 @@ var process = require('child_process');
 var fs = require('fs');
 var path = require('path');
 
+
+
 var amdclean = require('amdclean');
 var requirejs = require('requirejs');
 
 var stream = require('stream');
 
 var childProcess = require('child_process');
+
+var nodeList = require('../config.node.js');
+var browserList = require('../config.browser.js');
 
 var counterA = 0;
 
@@ -18,35 +23,100 @@ router.get('/', function (req, res, next) {
     counterA++;
     console.log('访问次数:' + counterA);
 
-    var moduleArr = req.query.module.split(',');
-    var min = req.query.select ? req.query.select : '';
+    // 先看有没有 type,如果有,则优先处理 type
+    var type = req.query.type;
+
+    console.log('type:' + type);
+
+    if (type) {
+        console.log('这里处理 type');
+
+        if (type == 'node') {
+            //res.writeHead(200, {"Access-Control-Allow-Origin": "*", "Content-Type": "text/html"});
+            //res.end("打包 node 平台下的库!" + nodeList);
+
+            var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
+
+            fileHandle.on('message', function (string) {
+
+                console.log('@@@' + string.substring(string.length - 20,string.length - 10));
+
+                // 写入返回请求
+                var rs = new stream.Readable;
+                rs.push(string);
+                rs.push(null);
+
+                res.writeHead(200, {
+                    'Content-Type': 'application/force-download',
+                    'Content-Disposition': 'attachment; filename=Utils.js',
+                    'location': '/'
+                });
+
+                rs.pipe(res);
+
+                fileHandle.kill();
+            });
+
+            fileHandle.send({'moduleArr':nodeList, 'min': 'dev'});
+
+        } else if (type == 'browser') {
+            var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
+
+            fileHandle.on('message', function (string) {
+
+                console.log('@@@' + string.substring(string.length - 20,string.length - 10));
+
+                // 写入返回请求
+                var rs = new stream.Readable;
+                rs.push(string);
+                rs.push(null);
+
+                res.writeHead(200, {
+                    'Content-Type': 'application/force-download',
+                    'Content-Disposition': 'attachment; filename=Utils.js',
+                    'location': '/'
+                });
+
+                rs.pipe(res);
+
+                fileHandle.kill();
+            });
+
+            fileHandle.send({'moduleArr':browserList, 'min': 'dev'});
+        }
+
+    } else {
+        var moduleArr = req.query.module.split(',');
+        var min = req.query.select ? req.query.select : '';
 
 
+        console.log(__dirname);
+        var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
 
-    console.log(__dirname);
-    var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
+        fileHandle.on('message', function (string) {
 
-    fileHandle.on('message', function (string) {
+            console.log('@@@' + string.substring(string.length - 20,string.length - 10));
 
-        console.log('@@@' + string.substring(string.length - 20,string.length - 10));
+            // 写入返回请求
+            var rs = new stream.Readable;
+            rs.push(string);
+            rs.push(null);
 
-        // 写入返回请求
-        var rs = new stream.Readable;
-        rs.push(string);
-        rs.push(null);
+            res.writeHead(200, {
+                'Content-Type': 'application/force-download',
+                'Content-Disposition': 'attachment; filename=Utils.js',
+                'location': '/'
+            });
 
-        res.writeHead(200, {
-            'Content-Type': 'application/force-download',
-            'Content-Disposition': 'attachment; filename=Utils.js',
-            'location': '/'
+            rs.pipe(res);
+
+            fileHandle.kill();
         });
 
-        rs.pipe(res);
+        fileHandle.send({'moduleArr':moduleArr, 'min': min});
+    }
 
-        fileHandle.kill();
-    });
 
-    fileHandle.send({'moduleArr':moduleArr, 'min': min});
 
 });
 
