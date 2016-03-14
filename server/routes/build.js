70143d2fcdaa0ee27a7e548dd30d12aa626cb6f3
leiquan <leiquan@live.com>
2016-03-14 17:36:41 +0800

    重新逻辑 node 和 browser

--- server/routes/build.js
+++ server/routes/build.js
@@ -11,6 +11,9 @@ var stream = require('stream');
 
 var childProcess = require('child_process');
 
+var nodeList = require('../config.node.js');
+var browserList = require('../config.browser.js');
+
 var counterA = 0;
 
 router.get('/', function (req, res, next) {
@@ -18,35 +21,85 @@ router.get('/', function (req, res, next) {
     counterA++;
     console.log('访问次数:' + counterA);
 
-    var moduleArr = req.query.module.split(',');
-    var min = req.query.select ? req.query.select : '';
+    if (req.query.type) {
+        if (req.query.type == 'node') {
+            var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
 
+            fileHandle.on('message', function (string) {
 
+                console.log('@@@' + string.substring(string.length - 20,string.length - 10));
 
-    console.log(__dirname);
-    var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
+                // 写入返回请求
+                var rs = new stream.Readable;
+                rs.push(string);
+                rs.push(null);
 
-    fileHandle.on('message', function (string) {
+                res.writeHead(200, {
+                    'Content-Type': 'application/force-download',
+                    'Content-Disposition': 'attachment; filename=Utils.js',
+                    'location': '/'
+                });
 
-        console.log('@@@' + string.substring(string.length - 20,string.length - 10));
+                rs.pipe(res);
 
-        // 写入返回请求
-        var rs = new stream.Readable;
-        rs.push(string);
-        rs.push(null);
+                fileHandle.kill();
+            });
 
-        res.writeHead(200, {
-            'Content-Type': 'application/force-download',
-            'Content-Disposition': 'attachment; filename=Utils.js',
-            'location': '/'
+            fileHandle.send({'moduleArr':nodeList, 'min': 'dev'});
+        } else if(req.query.type == 'browser'){
+            var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
+
+            fileHandle.on('message', function (string) {
+
+                console.log('@@@' + string.substring(string.length - 20,string.length - 10));
+
+                // 写入返回请求
+                var rs = new stream.Readable;
+                rs.push(string);
+                rs.push(null);
+
+                res.writeHead(200, {
+                    'Content-Type': 'application/force-download',
+                    'Content-Disposition': 'attachment; filename=Utils.js',
+                    'location': '/'
+                });
+
+                rs.pipe(res);
+
+                fileHandle.kill();
+            });
+
+            fileHandle.send({'moduleArr':nodeList, 'min': 'browser'});
+        }
+    } else {
+        var moduleArr = req.query.module.split(',');
+        var min = req.query.select ? req.query.select : '';
+
+        console.log(__dirname);
+        var fileHandle = childProcess.fork('./server/routes/fileHandle.js');
+
+        fileHandle.on('message', function (string) {
+
+            console.log('@@@' + string.substring(string.length - 20,string.length - 10));
+
+            // 写入返回请求
+            var rs = new stream.Readable;
+            rs.push(string);
+            rs.push(null);
+
+            res.writeHead(200, {
+                'Content-Type': 'application/force-download',
+                'Content-Disposition': 'attachment; filename=Utils.js',
+                'location': '/'
+            });
+
+            rs.pipe(res);
+
+            fileHandle.kill();
         });
 
-        rs.pipe(res);
-
-        fileHandle.kill();
-    });
-
-    fileHandle.send({'moduleArr':moduleArr, 'min': min});
+        fileHandle.send({'moduleArr':moduleArr, 'min': min});
+    }
 
 });
 
